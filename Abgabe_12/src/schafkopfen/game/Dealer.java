4ed5ac0b1872a8ae9567e0b1547082126bfc27d5
Lineax17 <philipp.hartmann04@proton.me>
2024-02-11 14:18:33 +0100

    Abgabe 12 nicht fertig

--- Abgabe_12/src/schafkopfen/game/Dealer.java
+++ Abgabe_12/src/schafkopfen/game/Dealer.java
@@ -1,5 +1,19 @@
 package schafkopfen.game;
 
+import schafkopfen.gameview.Animations;
+import schafkopfen.gameview.Card;
+
 public class Dealer {
-    
+    Animations animations;
+    Card[] deck;
+
+    Dealer() {
+        animations = new Animations();
+        deck = Card.createFreshDeckWith32Cards();
+    }
+
+    void play() {
+        animations.showCardDeckStraight(deck);
+
+    }
 }
